b6919bbab41af3ce7f2ca1cdaa6f779d8545590f
Mads Kristensen <madskristensen@users.noreply.github.com>
2019-07-16 14:47:10 -0700

    Updates

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -7,7 +7,7 @@ namespace HtmlTools
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "3cef2919-d8c7-4e9f-a809-5a0ba9c61fac";
+        public const string Id = "8682fae0-2ce9-49ea-93b5-edc92c118e2a";
         public const string Name = "HTML Tools 2019";
         public const string Description = @"Productivity tools for the HTML editor";
         public const string Language = "en-US";
